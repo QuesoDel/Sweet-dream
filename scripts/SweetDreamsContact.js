8dccc30e2d6d0a423c7acd58248e8e2d3342a1ea
QuesoDel <delgrosso.casey@student.ccm.edu>
2021-05-08 19:48:58 -0400

    Update SweetDreamsContact.js

--- scripts/SweetDreamsContact.js
+++ scripts/SweetDreamsContact.js
@@ -1,4 +1,9 @@
-map= new google.maps.Map(document.getElemrntById('map'). {
-center: {lat: 40.996747274416684, lng: -75.16235994413691},
-zoom: 8
-});
+var MyWindow;
+function openpopup()
+{
+    MyWindow = window.open('https://www.google.com/maps/place/373+E+Brown+St,+East+Stroudsburg,+PA+18301/@40.9967311,-75.1645701,17z/data=!3m1!4b1!4m5!3m4!1s0x89c4888719938851:0x83beb382974401a!8m2!3d40.9967311!4d-75.1623814', '_blank');
+}
+
+function closepopup() {
+    MyWindow.close();
+}
