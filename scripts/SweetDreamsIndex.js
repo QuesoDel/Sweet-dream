7000a4783d601c51c6a770f75e2fe2542647221b
QuesoDel <delgrosso.casey@student.ccm.edu>
2021-05-09 23:16:47 -0400

    Update SweetDreamsIndex.js

--- scripts/SweetDreamsIndex.js
+++ scripts/SweetDreamsIndex.js
@@ -1,2 +1,2 @@
-alert("Welcome to Sweet Dreams Sleep Center, feel free to navigate our home page and if you have any question our emial is at the bottom of all pages. Our goal here is for everyone to leave with sweet dreams. Thank you");
+alert("Welcome to Sweet Dreams Sleep Center, feel free to navigate our home page and if you have any question our email is at the bottom of all pages. Our goal here is for everyone to leave with sweet dreams. Thank you");
  
