2cea894c8037018ca4ccf8e82ae1c65cc7535562
QuesoDel <delgrosso.casey@student.ccm.edu>
2021-05-10 01:50:48 -0400

    Update SweetDreamsIndex.js

--- scripts/SweetDreamsIndex.js
+++ scripts/SweetDreamsIndex.js
@@ -1,3 +1,3 @@
 //This alert displays on the index page when first entering to help the patients navigate the page
-alert("Welcome to Sweet Dreams Sleep Center, feel free to navigate our home page and if you have any question our email is at the bottom of all pages. Our goal here is for everyone to leave with sweet dreams. Thank you!");
+alert("Welcome to Sweet Dreams Sleep Center, feel free to navigate our home page and if you have any question our email is at the bottom of all pages. Our goal here is for everyone to leave with sweet dreams. Thank you! :D");
  
